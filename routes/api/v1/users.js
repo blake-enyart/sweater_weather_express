f2df89cf81e3d69a778b25d88085b118e0fbbd5d
Blake <blake.enyart@gmail.com>
2019-06-26 22:20:35 -0600

    Add user endpoint

---
+++ routes/api/v1/users.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var usersController = require('../../../controllers/api/v1/users_controller.js');
+const { check, validationResult } = require('express-validator');
+
+/* Users CREATE */
+router.post('/',
+  [check('email').not().isEmpty().isEmail(),
+  check('password').isLength({ min: 6 })],
+
+  (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  usersController.create(req, res);
+});
+
+
+
+module.exports = router;
