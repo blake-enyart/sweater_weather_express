9cd29abf545c0abb08f414a280330bf4dc590312
Blake <blake.enyart@gmail.com>
2019-06-26 22:21:13 -0600

    Add routes for login

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var expressValidator = require('express-validator');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/v1/users');
+var sessionsRouter = require('./routes/api/v1/sessions');
 
 var app = express();
 
@@ -13,8 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(expressValidator());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/sessions', sessionsRouter);
 
 module.exports = app;
