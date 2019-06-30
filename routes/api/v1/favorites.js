97bb8e44dbb94811f5d669dc24bd7f8e440a99b7
Blake <blake.enyart@gmail.com>
2019-06-30 17:07:44 -0600

    Added index and create routes

---
+++ routes/api/v1/favorites.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var favoritesController = require('../../../controllers/api/v1/favorites_controller.js');
+
+router.post('/', (req,res) => {
+  favoritesController.create(req, res);
+})
+
+router.get('/', (req, res) => {
+  favoritesController.index(req, res);
+})
+
+module.exports = router;
